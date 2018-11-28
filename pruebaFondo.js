52798f36fced9dbb7114cb6f9bd623693a2e4096
eltiopasta <francisco.orozco1990@gmail.com>
2018-11-28 12:02:14 -0300

    Add files via upload

---
+++ pruebaFondo.js
@@ -0,0 +1,10 @@
+/* var obj = document.getElementById('region-main');
+ obj.style.backgroundColor = (obj.style.backgroundColor == 'rgb(204, 204, 204)') ? 'transparent' : '#CCCCCC';*/
+ 
+ 
+ var numero = 20;
+var nombre = "Juan";
+alert("atencion! su nombre es "+nombre+" y su edad es "+numero);
+
+
+console.log("El número es "+numero);
